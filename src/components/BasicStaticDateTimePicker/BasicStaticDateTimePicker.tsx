219ee4dbbe4c6dfe92051dd3694f107e4cb97b4c
AKot1988 <kulibabenko@ukr.net>
2024-08-01 00:03:39 +0300

    work process on new_user children route

--- src/components/BasicStaticDateTimePicker/BasicStaticDateTimePicker.tsx
+++ src/components/BasicStaticDateTimePicker/BasicStaticDateTimePicker.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
-import dayjs from 'dayjs';
+import { FC, useState, useEffect } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import { DateRange } from '@mui/x-date-pickers-pro/models';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
@@ -7,9 +8,11 @@ import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker';
 const date = new Date();
 
 const BasicStaticDateTimePicker: FC = () => {
+  const [value, setValue] = useState<DateRange<Dayjs>>([dayjs(date), dayjs(date)]);
+  useEffect(() => {console.log('value', value)}, [value]);
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <StaticDateTimePicker defaultValue={dayjs(date)} />
+          <StaticDateTimePicker defaultValue={dayjs(date)} onChange={(value) => setValue(value)} orientation="portrait" ampm={false}/>
     </LocalizationProvider>
   );
 }
