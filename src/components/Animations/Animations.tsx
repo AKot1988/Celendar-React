3ab7d9c2cd496757c88178ad5661a6b160e8405e
Kulibabenko Igorevich <kulibabenko@ukr.net>
2024-10-16 15:33:16 +0300

    styles up

--- src/components/Animations/Animations.tsx
+++ src/components/Animations/Animations.tsx
@@ -1,35 +1,50 @@
-export const textAnimation = {
-    hidden: { opacity: 0, x: -100},
-    visible: (custom: number) => ({
-      opacity: 1,
-      x: 0,
-      transition: {
-        delay: custom * 0.2,
-      },
-    }),
-  };
+import { duration } from "@mui/material";
+import { delay } from "framer-motion";
 
-  export const fadeTransparentInAnimation = {
-    hidden: { opacity: 0},
-    visible: (custom: number) => ({
-      opacity: 1,
-      transition: {
-        delay: custom * 0.2,
-      },
-    }),
-  };
+export const textAnimation = {
+  hidden: { opacity: 0, x: -100 },
+  visible: (custom: number) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: custom * 0.2,
+    },
+  }),
+};
+
+export const myFotoAnimation = {
+  hidden: { opacity: 0, x: 0 },
+  visible: (custom: number) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: custom * 0.2,
+      duration: 1,
+    },
+  }),
+};
+
+export const fadeTransparentInAnimation = {
+  hidden: { opacity: 0 },
+  visible: (custom: number) => ({
+    opacity: 1,
+    transition: {
+      delay: custom * 0.2,
+    },
+  }),
+};
 
 export const calendarAnimation = {
-    hidden: { opacity: 0, y: 100 },
-    visible: (custom: number) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.1,
-        delay: custom * 0.2,
-      },
-    }),
-}
+  hidden: { opacity: 0, y: 100 },
+  visible: (custom: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+      delay: custom * 0.2,
+    },
+  }),
+};
 
 export const rotateSVGs = {
   hidden: {
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { motion } from "framer-motion";
-import { textAnimation } from "../../components/Animations/Animations.tsx";
+import { textAnimation, myFotoAnimation } from "../../components/Animations/Animations.tsx";
 import classes from "./Contact.module.scss";
 
 const Contact: FC = () => {
@@ -13,7 +13,10 @@ const Contact: FC = () => {
       <motion.h1 custom={1} variants={textAnimation}>
         Contact
       </motion.h1>
-      <motion.iframe className={classes.contactPagePDF} src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonFiles%2FAnton_Kulibabenko_CV_FrontEnd%2015.10.2024.pdf?alt=media&token=bb5a5c09-23d9-404e-823d-2beea531c5c1"></motion.iframe>
+      <div className={classes.contactPageContent}>
+        <motion.img custom={5} variants={myFotoAnimation} className={classes.contactPageMyFace} src='https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonImages%2F20241008_155429_0_0_0_0-removebg.png?alt=media&token=bdcc6b4e-b62c-4f84-b3a3-c2a4211f5938'/>
+      </div>
+      {/* <iframe className={classes.contactPagePDF} src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonFiles%2FAnton_Kulibabenko_CV_FrontEnd%2015.10.2024.pdf?alt=media&token=bb5a5c09-23d9-404e-823d-2beea531c5c1"></iframe> */}
     </motion.div>
   );
 };
