7bada151cc0eb845ede9796b7ede5098316f7fde
Kulibabenko Igorevich <kulibabenko@ukr.net>
2024-10-17 14:15:43 +0300

    styles up

--- src/components/Animations/Animations.tsx
+++ src/components/Animations/Animations.tsx
@@ -19,6 +19,18 @@ export const myFotoAnimation = {
     x: 0,
     transition: {
       delay: custom * 0.2,
+      duration: 2,
+    },
+  }),
+};
+
+export const contactBlockAnimation = {
+  hidden: { opacity: 0, x: -200 },
+  visible: (custom: number) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: custom * 0.2,
       duration: 1,
     },
   }),
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import {
   textAnimation,
   myFotoAnimation,
+  contactBlockAnimation,
+  fadeTransparentInAnimation,
 } from "../../components/Animations/Animations.tsx";
 import classes from "./Contact.module.scss";
 
@@ -13,20 +15,41 @@ const Contact: FC = () => {
       whileInView="visible"
       className={classes.contactPage}
     >
-      <motion.h1 custom={1} variants={textAnimation}>
+      <motion.h1
+        custom={1}
+        variants={textAnimation}
+        className={classes.contactPageHeader}
+      >
         Contact
       </motion.h1>
       <div className={classes.contactPageContent}>
         <motion.img
           custom={5}
           variants={myFotoAnimation}
-          className={classes.contactPageMyFace}
-          src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonImages%2F20241008_155429_0_0_0_0-removebg.png?alt=media&token=bdcc6b4e-b62c-4f84-b3a3-c2a4211f5938"
+          className={classes.contactPageContentMyFace}
+          src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonImages%2FcropedWithoutBackground.png?alt=media&token=7db78761-36a6-476b-a70e-34f57f0f2c79"
         />
-        {/* <div className={classes.contactPageRegistrationBlock}>
-          <div className={classes.contactPageRegistrationBlockBackground}></div>
-          <h1>Варіанти зв'язку</h1>
-        </div> */}
+        <div className={classes.contactPageContentContactsBlock}>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={contactBlockAnimation}
+            className={classes.contactPageContentContactsBlockBackground}
+          ></motion.div>
+          <motion.h1 
+          initial="hidden"
+          whileInView="visible"
+          custom={2}
+          variants={fadeTransparentInAnimation}
+          className={classes.contactPageContentContactsBlockHeader}>
+            Варіанти зв'язку
+            <br />
+            <a rel="stylesheet" href="">
+              {" "}
+              посилання заглушка
+            </a>
+          </motion.h1>
+        </div>
       </div>
       {/* <iframe className={classes.contactPagePDF} src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonFiles%2FAnton_Kulibabenko_CV_FrontEnd%2015.10.2024.pdf?alt=media&token=bb5a5c09-23d9-404e-823d-2beea531c5c1"></iframe> */}
     </motion.div>
