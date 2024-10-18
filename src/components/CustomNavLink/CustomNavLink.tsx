a46fc725b164ec8a66e576dc35d95976d1be3b20
Kulibabenko Igorevich <kulibabenko@ukr.net>
2024-10-18 14:31:19 +0300

    styles up

--- src/components/CustomNavLink/CustomNavLink.tsx
+++ src/components/CustomNavLink/CustomNavLink.tsx
@@ -1,17 +1,35 @@
-import {FC} from 'react';
-import { NavLink } from 'react-router-dom';
+import { FC } from "react";
+import { NavLink } from "react-router-dom";
 
 export type CustomNavLinkProps = {
-  title: string
-  path: string
-  className: string
-  onClick?: ()=>void
-}
+  title: string;
+  path: string;
+  className?: string;
+  onClick?: () => void;
+  children?: JSX.Element;
+};
 
-const CustomNavLink: FC<CustomNavLinkProps> = ({title, path, className, onClick}) => {
+const CustomNavLink: FC<CustomNavLinkProps> = ({
+  title,
+  path,
+  className,
+  onClick,
+  children,
+}) => {
   return (
-      <NavLink to={path} className={className} onClick={onClick}>{title}</NavLink>
-  )
-}
+    <>
+      {children ? (
+        <NavLink to={path} className={className} onClick={onClick}>
+          {children}
+          {title}
+        </NavLink>
+      ) : (
+        <NavLink to={path} className={className} onClick={onClick}>
+          {title}
+        </NavLink>
+      )}
+    </>
+  );
+};
 
-export default CustomNavLink
+export default CustomNavLink;
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { CustomNavLink } from "../../components/index.tsx";
 import { motion } from "framer-motion";
 import {
   textAnimation,
@@ -6,6 +7,7 @@ import {
   contactBlockAnimation,
   fadeTransparentInAnimation,
 } from "../../components/Animations/Animations.tsx";
+import { contactsData } from "./helper.tsx";
 import classes from "./Contact.module.scss";
 
 const Contact: FC = () => {
@@ -23,12 +25,17 @@ const Contact: FC = () => {
         Contact
       </motion.h1>
       <div className={classes.contactPageContent}>
-        <motion.img
+        <motion.div
           custom={5}
           variants={myFotoAnimation}
-          className={classes.contactPageContentMyFace}
-          src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonImages%2FcropedWithoutBackground.png?alt=media&token=7db78761-36a6-476b-a70e-34f57f0f2c79"
-        />
+          className={classes.contactPageContentMyFaceContainer}
+        >
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonImages%2FcropedWithoutBackground.png?alt=media&token=7db78761-36a6-476b-a70e-34f57f0f2c79"
+            alt="my face"
+            className={classes.contactPageContentMyFaceImage}
+          />
+        </motion.div>
         <div className={classes.contactPageContentContactsBlock}>
           <motion.div
             initial="hidden"
@@ -36,19 +43,23 @@ const Contact: FC = () => {
             variants={contactBlockAnimation}
             className={classes.contactPageContentContactsBlockBackground}
           ></motion.div>
-          <motion.h1 
-          initial="hidden"
-          whileInView="visible"
-          custom={2}
-          variants={fadeTransparentInAnimation}
-          className={classes.contactPageContentContactsBlockHeader}>
-            Варіанти зв'язку
-            <br />
-            <a rel="stylesheet" href="">
-              {" "}
-              посилання заглушка
-            </a>
-          </motion.h1>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            custom={2}
+            variants={fadeTransparentInAnimation}
+            className={classes.contactPageContentContactsBlockLinks}
+          >
+            {contactsData.map((contact, index) => (
+              <CustomNavLink
+                key={index}
+                title={contact.title}
+                path={contact.path}
+                children={contact.children}
+                className={classes.contactPageContentContactsBlockLinksItem}
+              />
+            ))}
+          </motion.div>
         </div>
       </div>
       {/* <iframe className={classes.contactPagePDF} src="https://firebasestorage.googleapis.com/v0/b/calendar-react-85cff.appspot.com/o/commonFiles%2FAnton_Kulibabenko_CV_FrontEnd%2015.10.2024.pdf?alt=media&token=bb5a5c09-23d9-404e-823d-2beea531c5c1"></iframe> */}
